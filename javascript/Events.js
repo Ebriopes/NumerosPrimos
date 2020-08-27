31b2ba926f6cf4d08ab7770101c5f4ee8dce4778
Ebriopes <Antonio.vargasrosales@gmail.com>
2020-08-27 01:31:11 -0500

    group numbers

--- javascript/Events.js
+++ javascript/Events.js
@@ -1,11 +1,21 @@
 const input = document.getElementById('selPrime');
-console.info(typeof(input.value))
+let coll = document.getElementsByClassName("collapsible");
+
 input.addEventListener('keyup', event =>{
   if(event.keyCode === 13){
     event.preventDefault();
     document.getElementById('btn').click();
   }
 });
+
+collapse = (element) => {
+	let content = element.nextElementSibling;
+	if (content.style.maxHeight) {
+		content.style.maxHeight = "";
+	} else {
+		content.style.maxHeight = content.scrollHeight + "px";
+	}
+}
 
 setAmount = event => {
 	let amount = +input.value;
--- javascript/PrimeNumbers.js
+++ javascript/PrimeNumbers.js
@@ -1,47 +1,42 @@
-function primee(){
+function prime(limit) {
   //establecemos las variables
-  primeS = +document.getElementById('selPrime').value;
-  baseData = [2];
-  base = 3;
-  i=0;
+  const baseData = [2];
+  let base = 3;
 
-  //Comineza el Script
-  switch (true) {
-    //Si escojemos uno como opción
-    case (primeS === 1):
-      document.getElementById('res').innerHTML='The first Number is: ' + 2;
-	  break;
-	  
-    //si escojemos un número mayor a 1
-    case (primeS>1):
-      let primNum = '';
-      //bucle que cesa hasta tner la cantidad de digitos especificados
-      while(primeS > baseData.length){
-        //bucle que cesa cuando un numero no es primo
-        while(base % baseData[i] !== 0 ){
-          i++;
-          //condición que hace cambiar el número probado cuando se ha
-          //comprobado que es un número primo, y resetea el contador
-          if(i == baseData.length){
-            baseData.push(base);
-            i=0;
-            base += 2;
-          //Condición que sale del bucle cuando se alcanzó la cantidad 
-          //digitos solicitada
-          }else if(primeS == baseData.length){
-            break;
-          }
-        }
-        //Aumento en caso de que se pruebe un número no primo
-        base +=2;
-      };
-      for (const num in baseData) {
-        primNum += '<label>' + baseData[num] + '</label>  ';
-      };
-      document.getElementById('res').innerHTML='The Numbers are: <br> <div>' + primNum + '</div>';
-      break;
-    default:
-      document.getElementById('res').innerHTML='Apparently you didn\'t use numbers. :c';
+  //bucle que cesa hasta tner la cantidad de digitos especificados
+  for (let i = 0; limit > baseData.length; i++) {
+    //condición que hace cambiar el número probado cuando se ha
+    //comprobado que es un número primo, y resetea el contador
+    if (baseData[i] === undefined) {
+      baseData.push(base);
+      i = 1;
+      base += 2;
+    }
+    //Aumento en caso de que se pruebe un número no primo
+    if (base % baseData[i] === 0) base += 2;
   };
+
+  return baseData;
 };
 
+groupNum = (limit,group) => {
+  let primNum = '<div>';
+
+  prime(limit).forEach( (element,index) => {
+    if (index % group === 0) primNum += `</div><button class="collapsible" onclick="collapse(this)">${index}-${index+group}</button><div class="content">`;
+    primNum += `<span>${element}</span> `;
+  });
+
+  primNum += '</div>';
+  return primNum;
+}
+
+printNum = () => {
+  const limit = +document.getElementById('selPrime').value;
+  const group = 20;
+  if (limit === 1) {
+    document.getElementById('res').innerHTML = 'The first Number is: ' + 2;
+  } else {
+    res.innerHTML = `The Numbers are: <br> <div>${groupNum(limit,group)}</div>`;
+  }
+}
