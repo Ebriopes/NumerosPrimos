f064675b9f60b9374a7e60a8e2b9c4e617042026
Daniel Vargas <31797799+Ebriopes@users.noreply.github.com>
2025-09-05 22:00:16 -0600

    feature: ♻️ refactor prime generator logic
    Now the logic it's in a class

--- javascript/PrimeNumbers.js
+++ javascript/PrimeNumbers.js
@@ -1,42 +1,68 @@
-function prime(limit) {
-  //establecemos las variables
-  const baseData = [2];
-  let base = 3;
+class PrimeNumbers {
+    currentNumTry = 3
+    primeNumsFound = [2]
 
-  //bucle que cesa hasta tner la cantidad de digitos especificados
-  for (let i = 0; limit > baseData.length; i++) {
-    //condición que hace cambiar el número probado cuando se ha
-    //comprobado que es un número primo, y resetea el contador
-    if (baseData[i] === undefined) {
-      baseData.push(base);
-      i = 1;
-      base += 2;
+    #containerEl
+    #hostContainer
+    #limit = 1
+    #numberElements = []
+
+    constructor(hostContainer) {
+        this.#hostContainer = hostContainer
+        this.#containerEl = document.createElement('div')
+
+        this.#containerEl.className = 'numbers-container'
     }
-    //Aumento en caso de que se pruebe un número no primo
-    if (base % baseData[i] === 0) base += 2;
-  };
 
-  return baseData;
-};
+    set limit(num) {
+        this.#limit = num
+    }
 
-groupNum = (limit,group) => {
-  let primNum = '<div>';
+    *#findNextPrimeNumber() {
+        let isPrime
 
-  prime(limit).forEach( (element,index) => {
-    if (index % group === 0) primNum += `</div><button class="collapsible" onclick="collapse(this)">${index+1}-${index+group}</button><div class="content">`;
-    primNum += `<span>${element}</span> `;
-  });
+        do {
+            isPrime = this.primeNumsFound.every(
+                (num) => this.currentNumTry % num !== 0
+            )
 
-  primNum += '</div>';
-  return primNum;
-}
+            if (isPrime) {
+                this.primeNumsFound.push(this.currentNumTry)
 
-printNum = () => {
-  const limit = +document.getElementById('selPrime').value;
-  const group = 20;
-  if (limit === 1) {
-    document.getElementById('res').innerHTML = 'The first Number is: ' + 2;
-  } else {
-    res.innerHTML = `The Numbers are: <br> <div>${groupNum(limit,group)}</div>`;
-  }
+                yield this.currentNumTry
+                isPrime = false
+            }
+
+            this.currentNumTry += 2
+        } while (true)
+    }
+
+    #createNumberElement(num) {
+        const numContainer = document.createElement('div')
+
+        numContainer.className = 'num-container'
+        numContainer.innerHTML = `<span>${num}</span>`
+
+        return numContainer
+    }
+
+    showNumbers() {
+        if (this.#numberElements.length === 0) {
+            this.#hostContainer.className = 'show'
+            this.#hostContainer.append(this.#containerEl)
+            this.#numberElements.push(this.#createNumberElement(this.primeNumsFound[0]))
+        }
+
+        const findPrimeNumbers = this.#findNextPrimeNumber()
+
+        while (this.primeNumsFound.length <= this.#limit) {
+            const primeNum = findPrimeNumbers.next().value
+            const numEl = this.#createNumberElement(primeNum)
+            
+            this.#numberElements.push(numEl);
+            this.#containerEl.append(numEl)
+
+            setTimeout(() => numEl.className = 'num-container show', 0)
+        }
+    }
 }
